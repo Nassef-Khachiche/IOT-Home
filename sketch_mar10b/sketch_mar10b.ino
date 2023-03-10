e589a5430b72e61698cd1e1a1e11b9a21f274b78
JopFrenken <87734@roc-teraa.nl>
2023-03-10 09:34:40 +0100

    displays temp on btn click

--- sketch_mar10b/sketch_mar10b.ino
+++ sketch_mar10b/sketch_mar10b.ino
@@ -2,6 +2,8 @@
 #include "DHT.h"
 #define DHTPIN 4
 #define DHTTYPE DHT11
+#define LED_PIN 10
+#define BUTTON_PIN 2
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 DHT dht(DHTPIN, DHTTYPE);
@@ -13,14 +15,22 @@ void setup() {
   lcd.init();
   lcd.clear();
   lcd.backlight();
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT);
 }
 
 void loop() {
-  // read humidity
-  humi  = dht.readHumidity();
-  // read temperature as Celsius
-  tempC = dht.readTemperature();
-  lcd.print(tempC);
-  delay(1500);
-  lcd.clear();
+    // read humidity
+    humi  = dht.readHumidity();
+    // read temperature as Celsius
+    tempC = dht.readTemperature();
+
+    if (digitalRead(BUTTON_PIN) == LOW) {
+      lcd.clear();
+      digitalWrite(LED_PIN, HIGH);
+      lcd.print(tempC);
+    }
+    else {
+      digitalWrite(LED_PIN, LOW);
+    }
 }

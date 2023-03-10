b07e78dd76c4e58b6b2215e7b521ab8470725e82
JopFrenken <87734@roc-teraa.nl>
2023-03-10 09:29:54 +0100

    temp + humi

--- sketch_mar10b/sketch_mar10b.ino
+++ sketch_mar10b/sketch_mar10b.ino
@@ -1,15 +1,26 @@
 #include <LiquidCrystal_I2C.h>
+#include "DHT.h"
+#define DHTPIN 4
+#define DHTTYPE DHT11
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);
-
+DHT dht(DHTPIN, DHTTYPE);
+float humi;
+float tempC;
 
 void setup() {
-
+  dht.begin();
   lcd.init();
   lcd.clear();
   lcd.backlight();
-  lcd.print("hallo abrar");
 }
 
 void loop() {
+  // read humidity
+  humi  = dht.readHumidity();
+  // read temperature as Celsius
+  tempC = dht.readTemperature();
+  lcd.print(tempC);
+  delay(1500);
+  lcd.clear();
 }
